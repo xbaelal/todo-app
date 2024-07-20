6c71d880a2844983b660217a32ff5181143f214d
Bilal <memonbilal760@gmail.com>
2024-07-20 16:48:59 +0500

    footer added

--- app.js
+++ app.js
@@ -9,19 +9,8 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
-const h1Task = document.getElementById("h1task")
-
 const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
-
-const hideH1 = () => {
-  h1Task.setAttribute("class", "h1taskHid")
-};
-
-const showH1 = () => {
-  h1Task.removeAttribute("class", "h1taskHid")
-  h1Task.setAttribute("class", "h1task")
-}
 
 const addOrUpdateTask = () => {
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
@@ -34,7 +23,6 @@ const addOrUpdateTask = () => {
 
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
-    hideH1()
   } else {
     taskData[dataArrIndex] = taskObj;
   }
@@ -71,9 +59,6 @@ const deleteTask = (buttonEl) => {
   buttonEl.parentElement.remove();
   taskData.splice(dataArrIndex, 1);
   localStorage.setItem("data", JSON.stringify(taskData));
-  if(!taskData.length){
-    showH1()
-  }
 }
 
 const editTask = (buttonEl) => {
